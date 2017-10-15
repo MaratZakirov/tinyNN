01e73c2d7aabfc46bf5d8e380b0709d47fc1d9ab
MaratZakirov <marat61@gmail.com>
2017-10-16 01:14:15 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,9 +1,35 @@
 import numpy
 import time
+import math
+
+from scipy import misc
+import matplotlib.pyplot as plt
+
+f = misc.face(gray=True)
+plt.imshow(f, cmap='gray')
+plt.show()
+F = numpy.fft.fft2(f)
+I2 = numpy.absolute(F)
+I2 = I2 / 1000000
+plt.imshow(I2, cmap='gray')
+plt.show()
+exit()
+
+from PIL import Image
+import numpy as np
+
+w, h = 512, 512
+data = np.zeros((h, w, 3), dtype=np.uint8)
+data[256, 256] = [255, 0, 0]
+img = Image.fromarray(data, 'RGB')
+#img.save('my.png')
+img.show()
+
+exit()
 
 def myown_fft(x):
     x = x.T
-    assert x.shape[0] in set([2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 1024 * 2, 1024 * 4])
+    assert int(math.log(x.shape[0], 2)) == math.log(x.shape[0], 2)
     def fft_rec(x):
         X = numpy.zeros(shape=x.shape, dtype='complex64')
         if x.shape[0] == 2:
@@ -20,13 +46,9 @@ def myown_fft(x):
     return fft_rec(x).T
 
 def myown_fft2(x):
-    p2 = set([2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 1024 * 2, 1024 * 4])
-    assert x.shape[0] in p2 and x.shape[1]
-    X = myown_fft(x)
-    Y = myown_fft(X.T)
-    return Y
+    return myown_fft(myown_fft(x).T).T
 
-if 1:
+if 0:
     I = numpy.random.uniform(size=(1024 * 4, 1024 * 4))
     a = time.clock()
     #numpy.fft.fft2(I)
@@ -34,20 +56,17 @@ if 1:
     print(a, time.clock())
     exit()
 
-if 1:
+if 0:
     x = numpy.array([0, 1, 2, 3, 0, 1, 5, 8])
     X = numpy.fft.fft(x)
     print(X)
     X = myown_fft(x)
     print(X)
 
-if 0:
+if 1:
     ### fft od 2d image
     x = numpy.array([[8, 2, 6, 1],
-                     [3, 4, 7, 1],
-                     [6, 2, 8, 3],
-                     [1, 1, 2, 2]])
-    #x = numpy.array([[4, 5], [2, 1]])
+                     [3, 4, 7, 1]])
     X = numpy.fft.fft2(x)
     print(X)
     X = myown_fft2(x)
